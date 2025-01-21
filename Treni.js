77a1dbdc2c6b23b8be44c05103146350fd9e9f5c
IgorGoro91 <igorysc@gmail.com>
2025-01-21 18:01:44 +0100

    add:correzione

--- Treni.js
+++ Treni.js
@@ -3,12 +3,14 @@ const prezzo = 0.21
 
 btn.addEventListener(`click`, function(){
     
-
+    
     const nome = document.getElementById(`nome`).value
     const km = parseInt(document.getElementById(`km`).value)
+
+    
     const eta = document.getElementById(`fascia-eta`).value
 
-    console.log(nome, km, eta)
+    
     
     const passeggero = document.getElementById(`passeggero`)
 
@@ -18,10 +20,12 @@ btn.addEventListener(`click`, function(){
     const carroza = document.getElementById(`carroza`)
     const codice = document.getElementById(`codice`)
     const prezzo = document.getElementById(`prezzo`)
+    console.log(eta)
 
     if( eta == "maggiorenne"){
         offerta.innerHTML = " Biglietto standart"
         prezzo.innerHTML = (prezzo * km).toFixed(2)
+        console.log(prezzo, offerta)
     }else if ( eta == "minorenne"){
         offerta.innerHTML = "Bigletto ridotto"
         prezzo.innerHTML = ((prezzo * km) * 0.80).toFixed(2)
